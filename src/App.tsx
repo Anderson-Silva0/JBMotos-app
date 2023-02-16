e5dd4a3db8bb659d0fa80241d5586a22528f7a37
Anderson-Silva0 <andersonsilvaa0@outlook.com>
2023-02-15 22:29:21 -0300

    Componente de Rota adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react" 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/navbar"
+import 'toastr/build/toastr.css'
+import 'toastr/build/toastr.min.js'
+import { Rotas } from './components/rotas';
 
 function App() {
-
   return (
     <div>
-      
+      <NavBar />
+      <Rotas />
     </div>
   )
 }
